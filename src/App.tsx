ed54388283180a03c985aa9a8fa17b6fe7c0afe3
ELENA PETROVA <111912799+Elena-1405@users.noreply.github.com>
2024-06-24 16:20:34 +0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,112 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { Calendar, CalendarProps } from '../src/components/calendar/calendar';
+import { TodoList, ToDo} from '../src/components/list/todolist';
+import { Modal } from '../src/components/modal/modal';
+import { startOfMonth, endOfMonth, eachDayOfInterval } from '../src/utils/dates';
+import isDayOff from 'isdayoff';
+
+const users = ['user1', 'user2'];
 
 function App() {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [ todos, setTodos ] = useState<{ [key: string]: {[date: string]: ToDo[] }}>({
+    user1: {},
+    user2: {},
+  });
+  const [currentUser, setCurrentUser] = useState<string>('user1');
+  const [ isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [ holidays, setHolidays ] = useState<{ [key: string]: boolean}>({});
+
+
+  useEffect(() => {
+    localStorage.setItem(`todos_${currentUser}`, JSON.stringify(todos));
+  }, [todos, currentUser]);
+
+  useEffect(() => {
+    const savedTodos = localStorage.getItem(`todos_${currentUser}`);
+    if (savedTodos) {
+      setTodos(JSON.parse(savedTodos));
+    }
+  }, [currentUser]);
+
+  useEffect(() => {
+    const fetchHolidays = async () => {
+      const start = startOfMonth(selectedDate);
+      const end = endOfMonth(selectedDate);
+      const days = eachDayOfInterval(start, end);
+      const holidaysData: { [key: string]: boolean } = {};
+
+      for (const day of days) {
+        const dayString = day.toISOString().split('T')[0];
+        const isHoliday = await isDayOff(dayString, { country: 'ru' });
+        holidaysData[dayString] = isHoliday;
+      }
+
+      setHolidays(holidaysData);
+    };
+
+    fetchHolidays();
+  }, [selectedDate]);
+
+  const addTodo = (date: Date, todo: ToDo) => {
+    const dateString = date.toISOString().split('T')[0];
+    setTodos((prevTodos) => ({
+      ...prevTodos, [currentUser]: {
+        ...prevTodos[currentUser],
+        [dateString]: [...(prevTodos[currentUser][dateString] || []), todo],
+      }  
+    }));
+  };
+
+  const removeTodo = (date: Date, index: number) => {
+    const dateString= date.toISOString().split('T')[0];
+    setTodos((prevTodos) => {
+      const userTodos = {...prevTodos[currentUser]};
+      if (userTodos[dateString]) {
+        userTodos[dateString] = userTodos[dateString].filter((_, i) => i !== index);
+      }
+      return { ...prevTodos, [currentUser]: userTodos };
+    });
+  };
+
+  const toggleModal = (date: Date) => {
+    setSelectedDate(date);
+    setIsModalOpen(!isModalOpen);
+  }
+
+  const handleUserChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const newUser = e.target.value;
+    setCurrentUser(newUser);
+    localStorage.setItem('currentUser', newUser);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>Календарь и список задач</h1>
+    <select value={currentUser} onChange={handleUserChange}>
+      {users.map((user) => (
+        <option key={user} value={user}>
+          {user}
+        </option>
+      ))}
+    </select>
+      <Calendar 
+        selectedDate={selectedDate} 
+        onDateClick={toggleModal} 
+        todos={todos[currentUser]}
+        holidays={holidays}
+      />
+      {isModalOpen && (
+        <Modal onClose={() => setIsModalOpen(false)}>
+          <TodoList 
+            date={selectedDate}
+            todos={todos[currentUser][selectedDate.toISOString().split('T')[0]] || []} 
+            addTodo={(todo) => addTodo(selectedDate, todo)} 
+            removeTodo={(index) => removeTodo(selectedDate, index)}
+          />
+      </Modal>
+      )}  
+    </>
   );
 }
 
